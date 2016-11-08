95f212b72abdbe4c735f779549507b43cf8eefdd
Alexander <220619@gmail.com>
2016-11-09 02:23:32 +0300

    hui

--- public/js/js.js
+++ public/js/js.js
@@ -4,6 +4,13 @@ $(document).ready(function() {
         fitToSection: false,
         fitToSectionDelay: 500,
         scrollBar: true,
-        sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#000']
+        sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#4BBFC3'],
+        //Navigation
+        lockAnchors: false,
+        anchors:['p1', 'p2', 'p3', 'p4'],
+        navigation: true,
+        navigationPosition: 'right',
+        navigationTooltips: ['firstSlide', 'secondSlide'],
+        showActiveTooltip: false
     });
 });

e51a507e166fab643604f3a42f8e493845fff7d5
Alex <a.ksents@smstraffic.ru>
2017-01-10 19:00:37 +0300

    hui

--- resources/views/common/mainNav.blade.php
+++ resources/views/common/mainNav.blade.php
@@ -10,14 +10,24 @@
         <div class="l social-links">
             <span>в соцсетях:
                 <a href="https://vk.com/fitnespraktika" title="ВКонтакте" class="fa fa-vk" target="_blank"></a>
+                <a href="https://www.instagram.com/fitnespraktika" title="Instagram" class="fa fa-instagram" target="_blank"></a>
+                <a href="https://www.youtube.com/channel/UCZIsiLeWkbynnERIgYIrYBg" title="YouTube" class="fa fa-youtube" target="_blank"></a>
+                <a href="https://www.facebook.com/fitnespraktika" title="Facebook" class="fa fa-facebook-square" target="_blank"></a>
+                <a href="https://telegram.me/fitnespraktika_bot" title="Telegram (бот)" class="fa fa-telegram" target="_blank"></a>
             </span>
         </div>
 
         <div class="r menu">
             <nav>
-                <span><a href="/calendar">Point</a></span>
-                <span><a href="/calendar">Point</a></span>
-                <span><a href="/calendar">Point</a></span>
+                <span><a href="{{ route('calendar:index', [], false) }}">Календарь</a></span>
+                <span><a href="{{ route('videos:index', [], false) }}">Видеоотчёты</a></span>
+                <span><a href="{{ route('gallery:index', [], false) }}">Фотоотчёты</a></span>
+                @if(!empty($currentUser))
+                    <span><img class="profile-picture-img" src="/images/thumbs/{{ $currentUser->getThumbnail() }}.jpg" title="Редактировать профиль" /> <a href="{{ route('my:index', [], false) }}">Профиль</a></span>
+                    <span><a href="{{ route('logout', [], false) }}">Выйти</a></span>
+                @else
+                   <span><i class="fa fa-unlock"></i><a href="{{ route('login', [], false) }}" rel="nofollow">Войти</a></span>
+                @endif
             </nav>
         </div>
     </div>

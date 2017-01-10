b9d0712e1b77f0ec5353fbca891aa6b4d3966df4
Alex <a.ksents@smstraffic.ru>
2017-01-10 17:58:24 +0300

    responsive main nav

--- resources/views/common/mainNav.blade.php
+++ resources/views/common/mainNav.blade.php
@@ -1,16 +1,19 @@
 <div class="main-nav section">
     <div class="wrapper clearfix">
-        <div class="l">
-            <span><a href="{{ route('index', [], false) }}">Index</a></span>
+        <div class="l index">
+            <span>
+                <a href="{{ route('index', [], false) }}">Index</a>
+                <a onclick="$('.main-nav .menu').toggle();" class="responsive-menu-control fa fa-bars" href="javascript:void(0);"></a>
+            </span>
         </div>
 
-        <div class="social-links l">
+        <div class="l social-links">
             <span>в соцсетях:
                 <a href="https://vk.com/fitnespraktika" title="ВКонтакте" class="fa fa-vk" target="_blank"></a>
             </span>
         </div>
 
-        <div class="r">
+        <div class="r menu">
             <nav>
                 <span><a href="/calendar">Point</a></span>
                 <span><a href="/calendar">Point</a></span>
